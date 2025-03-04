f644bb6be8cf323c7e1ff69b76d176f1d978dfca
nagarutu <naga@n4ga.net>
2025-03-04 23:33:21 +0100

    more changes

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,5 +1,5 @@
 return {
-{
+	{
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
@@ -30,8 +30,8 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
 			lspconfig.ansiblels.setup({ capabilities = capabilities })
 			lspconfig.bashls.setup({ capabilities = capabilities })
@@ -48,7 +48,7 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover information" })
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Go to references" })
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {desc = "Code Action" })
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
 		end,
 	},
 	{
@@ -58,7 +58,7 @@ return {
 				ensure_installed = {
 					"stylua",
 					"black",
-					"eslint",
+					"eslint_d",
 					"prettier",
 					"isort",
 				},
@@ -72,11 +72,11 @@ return {
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
-					null_ls.builtins.diagnostics.black,
+				  null_ls.builtins.formatting.black,
 					null_ls.builtins.formatting.isort,
-					null_ls.builtins.diagnostics.eslint,
 					null_ls.builtins.formatting.prettier,
-				},
+          -- null_ls.builtins.diagnostics.eslint_d
+        },
 			})
 			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format" })
 		end,
