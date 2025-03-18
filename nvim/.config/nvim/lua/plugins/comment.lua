b949dfd45550b917ed820b35e8d76142b01a68a9
nagarutu <naga@n4ga.net>
2025-03-18 15:10:20 +0100

    Fixed nvim comment

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,7 +1,8 @@
 return {
 	"numToStr/Comment.nvim",
 	lazy = false, -- or true to lazy load on require if desired.
-	config = function()
-		vim.keymap.set( "n", "<leader>/", "<Plug>(comment_toggle_linewise_current)", { desc = "Toggle comment linewise" })
+  config = function()
+		require('Comment').setup()
+    vim.keymap.set( "n", "<leader>/", "<Plug>(comment_toggle_linewise_current)", { desc = "Toggle comment linewise" })
 	end,
 }
