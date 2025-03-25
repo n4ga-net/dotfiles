dcd528e1290160945581024401131bc702b34456
nagarutu <naga@n4ga.net>
2025-03-25 22:57:34 +0100

    nord for hyprland and minty attempt

---
+++ nvim/.config/nvim/lua/plugins/minty.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvzone/minty",
+    config = function()
+      require("minty").setup({
+        show_palette = true,
+        copy_on_select = true,
+        use_border = true,
+      })
+    end,
+  },
+  {
+    "nvzone/volt",
+    lazy = true
+  },
+}
