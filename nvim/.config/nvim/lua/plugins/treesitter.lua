b3ace7416c7b106bb615a9bd930a9e1def6b51c9
naga <naga@n4ga.net>
2025-12-31 16:57:39 +0100

    updated nvimtreesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,61 +1,43 @@
--- treesitter.lua
 return {
-  "nvim-treesitter/nvim-treesitter",
-  version = false, -- use latest
-  build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    ensure_installed = {
-      "markdown",
-      "bash",
-      "powershell",
-      "python",
-      "json",
-      "xml",
-      "yaml",
-      "lua",
-      "vim",
-      "dockerfile",
-      "zsh",
-      "html",
-      "css",
-      "javascript",
-      "typescript"
-    },
-    sync_install = false,        -- install parsers asynchronously
-    auto_install = true,         -- install missing parsers on buffer enter
-    highlight = {
-      enable = true,             -- false will disable the whole extension
-      additional_vim_regex_highlighting = false,
-    },
-    indent = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "gnn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-        },
-      },
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+	ensure_installed = {
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "markdown",
+          "markdown_inline",
+          "bash",
+          "powershell",
+          "python",
+          "json",
+          "xml",
+          "yaml",
+          "lua",
+          "vim",
+          "dockerfile",
+          "html",
+          "css",
+          "javascript",
+          "typescript"
   },
-
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+	auto_install = true,
+	highlight = {
+	  enable = true,
+	  disable = function(buf)
+	    local max_filesize = 100 * 1024 -- 100 KB
+	    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	    if ok and stats and stats.size > max_filesize then
+	      return true
+	    end
+	  end,
+	  additional_vim_regex_highlighting = false,
+	},
+      }
+    end,
+  }
 }
-
