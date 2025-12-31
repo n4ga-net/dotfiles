ab5c3f81324132eaacde668dcbf94b7120efb1ce
naga <naga@n4ga.net>
2025-12-31 22:00:20 +0100

    treesitter update

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,33 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-			ensure_installed = {
-				"markdown",
-				"markdown_inline",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"powershell",
-				"python",
-				"json",
-				"xml",
-				"yaml",
-				"lua",
-				"vim",
-				"dockerfile",
-				"html",
-				"css",
-				"javascript",
-				"typescript"
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+
+  -- This makes Lazy do: require("nvim-treesitter.configs").setup(opts)
+  main = "nvim-treesitter.configs",
+  opts = {
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "bash",
+      "css",
+      "dockerfile",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "powershell",
+      "python",
+      "query",
+      "typescript",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+    },
+  },
 }
+
