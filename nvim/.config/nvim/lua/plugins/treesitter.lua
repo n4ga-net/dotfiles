a645d4630ef3be947ca89d027bce6806709853a9
naga <naga@n4ga.net>
2025-12-31 17:29:52 +0100

    formatting

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,44 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      require'nvim-treesitter.configs'.setup {
-	  ensure_installed = {
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "markdown",
-          "markdown_inline",
-          "bash",
-          "powershell",
-          "python",
-          "json",
-          "xml",
-          "yaml",
-          "lua",
-          "vim",
-          "dockerfile",
-          "html",
-          "css",
-          "javascript",
-          "typescript"
-  },
-	auto_install = true,
-	highlight = {
-	  enable = true,
-	  disable = function(buf)
-	    local max_filesize = 100 * 1024 -- 100 KB
-	    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-	    if ok and stats and stats.size > max_filesize then
-	      return true
-	    end
-	  end,
-	  additional_vim_regex_highlighting = false,
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"markdown",
+					"markdown_inline",
+					"bash",
+					"powershell",
+					"python",
+					"json",
+					"xml",
+					"yaml",
+					"lua",
+					"vim",
+					"dockerfile",
+					"html",
+					"css",
+					"javascript",
+					"typescript",
+				},
+				auto_install = true,
+				highlight = {
+					enable = true,
+					disable = function(buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
 	},
-      }
-    end,
-  }
 }
