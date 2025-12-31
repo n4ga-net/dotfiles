db9fff3e098dcd1adf4dcc7f042f516028f6144c
naga <naga@n4ga.net>
2025-12-31 16:35:55 +0100

    Updates

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,52 @@
+-- treesitter.lua
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-      ensure_installed = {
-        "markdown", "markdown_inline",
+  "nvim-treesitter/nvim-treesitter",
+  version = false, -- use latest
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    ensure_installed = {
+      "markdown",
+      "bash",
+      "powershell",
+      "python",
+      "json",
+      "xml",
+    },
+    sync_install = false,        -- install parsers asynchronously
+    auto_install = true,         -- install missing parsers on buffer enter
+    highlight = {
+      enable = true,             -- false will disable the whole extension
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
+    },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
       },
-		})
-	end,
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+        },
+      },
+    },
+  },
+
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
 }
+
