771d6243f17f853a2f9bf2e4a66d6ae3180e6684
naga <naga@n4ga.net>
2026-01-06 22:34:33 +0100

    updates

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -3,7 +3,6 @@ return {
   config = function()
     -- disable Copilot's default <Tab> mapping
     vim.g.copilot_no_tab_map = true
-
     -- map <C-a> to accept Copilot suggestion
     vim.keymap.set("i", "<C-a>", 'copilot#Accept("<CR>")', {
       silent = true,
